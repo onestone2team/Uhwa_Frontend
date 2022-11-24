43930154aac867f15fb5fd18f462f4b8cbd1d14a
JoonPyoJang <jjpyo1009@gmail.com>
2022-11-24 14:11:40 +0900

    feat:profile 페이지 UI 수정

--- static/js/profile.js
+++ static/js/profile.js
@@ -29,3 +29,54 @@ function myOrderList(){
     gallery_mypage.style.display ="none";
     gallery_bookmark.style.display ="none";
 }
+
+function editProfile(){
+    
+    const profilename = document.getElementById("author_name").value;
+    const password = document.getElementById("password").value;
+    const phone = document.getElementById("phone").value;
+    const address = document.getElementById("address").value;
+    const profile = document.querySelector("input[type='file']");
+    let formData = new FormData();
+    if (profile.value != "") {
+        formData.append('profile', profile.files[0]);
+    }
+    formData.append("profilename", profilename);
+    formData.append("password", password);
+    formData.append("phone", phone);
+    formData.append("address", address);
+
+    alert("수정 버튼 클릭");
+    location.reload();
+}
+
+// 모달 팝업창 작업
+
+const modal = document.querySelector('.modal');
+const btnOpenPopup = document.querySelector('#editButton');
+
+btnOpenPopup.addEventListener('click', () => {
+    modal.style.display = 'block';
+});
+
+const buttonCloseModal = document.getElementById("close_modal");
+buttonCloseModal.addEventListener("click", e => {
+    modal.style.display = "none";  
+    document.body.style.overflowY = "visible";
+});
+
+
+$("#input_image").change(function(){
+    readFile(this);
+    
+});
+
+function readFile(input_image){
+    var reader = new FileReader();
+  
+    reader.onload = function(e){
+        $('#output_image').attr('src', e.target.result);
+    }
+
+    reader.readAsDataURL(input_image.files[0]);
+} 
