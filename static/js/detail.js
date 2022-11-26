50c6e63b9dc9d2cd1628c4b6eff9b278438b78ba
hyojine <touroz@naver.com>
2022-11-26 21:35:15 +0900

    feat: 상세페이지 댓글 수정 및 삭제/ 주문 기능 추가

--- static/js/detail.js
+++ static/js/detail.js
@@ -5,25 +5,22 @@ var amount_money = 0
 var inputnum = document.getElementById("countNum");
 var moneynum = document.getElementById("amout_money");
 
-window.onload = () => {
-    $("#headers").load("../templates/navigation.html");
-    inputnum.innerText = num;
-    moneynum.innerText = 0;
-}
+// window.onload = () => {
+//     $("#headers").load("../templates/navigation.html");
+//     inputnum.innerText = num;
+//     moneynum.innerText = 0;
+// }
 
-let url = window.location.search;
-console.log(url)
-let urlparam = new URLSearchParams(url);
-console.log(urlparam)
-// var product_id = urlParameter.split('=')[1]
-var product_id = urlparam.get("product_id")
-var comment_id = urlparam.get('comment_id')
+let urlParameter = window.location.search;
+var product_id = urlParameter.split('=')[1]
 console.log(product_id)
-console.log(comment_id)
 
 //=======게시글 불러오기========
 window.onload = 
 async function ProductDetail() {
+    $("#headers").load("../templates/navigation.html");
+    inputnum.innerText = num;
+    moneynum.innerText = 0;
 
     const product = await fetch(`${BACK_END_URL}/products/${product_id}/detail/`, {
         headers: {
@@ -34,7 +31,6 @@ async function ProductDetail() {
     })
 
 //========게시글 이미지 불러오기========
-
     product_json = await product.json()
     console.log(product_json)
 
@@ -49,33 +45,44 @@ async function ProductDetail() {
 
     comments.forEach(element => {
         const comment = document.createElement('div')
-        comment.innerHTML =`<div class="comment">
-                            <!-- Comment Avatar -->
-                            <div class="comment-avatar">
-                            <img src="${BACK_END_URL}${element.user.profile}" style="height:100%">
-                            </div>
-                            <!-- Comment Box -->
-                            <div class="comment-box" >
-                                <div class="comment-text">${element.comment}</div>
-                                    <div class="comment-footer">
-                                        <div class="comment-info">
-                                            <span class="comment-author">
-                                            <a>${element.user.profilename}</a>
-                                            </span>
-                                            <span class="comment-date">${element.created_at}</span>
-                                            <span>|</span>
-                                            <span classs="comment-grade"> 점수 : ${element.grade}</span>
-                                        </div>
-                                        <div class="comment-actions" id="edit-button">
-                                            <i onclick="editComment()" class="bi bi-pencil-square"></i>
-                                            <span>/</span>
-                                            <i onclick="deleteComment()" class="bi bi-trash3"></i>
+        comment.innerHTML =`<!-- Comments List -->
+                            <div class="comments" id="commment">
+                                <!-- Comment - Dummy -->
+                                <div class="comment">
+                                    <!-- Comment Avatar -->
+                                    <div class="comment-avatar">
+                                    <img src="${BACK_END_URL}${element.user.profile}" style="height:100%">
+                                    </div>
+                                    <!-- Comment Box -->
+                                    <div class="comment-box" >
+                                        <div class="comment-text">${element.comment}</div>
+                                            <div class="comment-footer">
+                                                <div class="comment-info">
+                                                    <span class="comment-author">
+                                                    <a>${element.user.profilename}</a>
+                                                    </span>
+                                                    <span class="comment-date">${element.created_at}</span>
+                                                    <span>|</span>
+                                                    <span classs="comment-grade"> 점수 : ${element.grade}</span>
+                                                </div>
+                                                <div class="comment-actions" id="edit-button">
+                                                    <i onclick="editComment(${element.id})" class="bi bi-pencil-square"></i>
+                                                    <span>/</span>
+                                                    <i onclick="deleteComment(${element.id})" class="bi bi-trash3"></i>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
+                                    <!-- Comment Edit -->
+                                    <div class="comment-edit" id=edit_box>
+                                        <textarea class="input" name="comment_edit" id="comment_edit" ng-model="cmntCtrl.comment.text"
+                                        placeholder="Edit Review..." required></textarea>
+                                        <div class="edit_button">
+                                            <button type="button" onclick="putComment(${element.id})">add Review</button>
+                                        </div> 
+                                    </div>
                                 </div>
-                                </div> 
                             </div>`
-
         comment_frame.appendChild(comment)
     })
 
@@ -108,31 +115,42 @@ async function addComment(){
         location.reload()
     }
 //==========댓글 수정하기==========
-// async function editComment() {
-//         console.log("댓글 수정")
-//         const editBox = document.getElementById('comment_edit').value
-//         const response = await fetch(`${BACK_END_URL}/products/${product_id}/comment/${comment_id}/`, {
-//             headers: {
-//                 "content-type": "application/json",
-//                 "Authorization": "Bearer " + localStorage.getItem("access")
-//             },
-//             method: "Update",
-//             body: JSON.stringify({
-//                 "comment": editBox,
-//             })
-//         })
-//         if (response.status == 200) {
-//             alert(response.data)
-//         }
-//         else {
-//             alert(response.status)
-//         }
+function editComment() {
+    console.log("댓글 수정")
+    const editBox = document.getElementById('edit_box')
+    if (editBox.style.display == 'none') {
+        editBox.style.display = 'block';
+    } else {
+        editBox.style.display = 'none';
+    }
 
-//         location.reload()
+}
 
-// }
+async function putComment(comment_id) {
+    const new_comment = document.getElementById('comment_edit').value
+    console.log(new_comment)
+        const response = await fetch(`${BACK_END_URL}/products/${product_id}/comment/${comment_id}/`, {
+            headers: {
+                "content-type": "application/json",
+                "Authorization": "Bearer " + localStorage.getItem("access")
+            },
+            method: 'PUT',
+            body: JSON.stringify({
+                "comment": new_comment
+            })
+        }).then(respond =>{
+            if (respond.status == 200) {
+                alert(respond.data)
+            }
+            else {
+                alert(respond.status)
+            }
+            location.reload()
+        })
+}
+
 //==========댓글 삭제하기==========
-async function deleteComment(){
+async function deleteComment(comment_id){
 
     const url = `${BACK_END_URL}/products/${product_id}/comment/${comment_id}/`;
     console.log(url)
@@ -147,7 +165,7 @@ async function deleteComment(){
                                             alert(respond.status);
                                         }
             else {
-                    alert("삭제되지 않았습니다 다시해")
+                    alert("댓글이 삭제되지 않았습니다. 다시 시도해주세요")
                  }
 
     const target = document.querySelector(".comment");
@@ -176,6 +194,38 @@ async function bookmark(product_id) {
 
     }
 
+async function orderButton() {
+        console.log("주문 버튼 클릭")
+        console.log(num, size, amount_money)
+        console.log(String(num))
+        // var snum=String(num)
+        // var ssize=String(size)
+        // var samount_money=String(amount_money)
+        // var id=String(product_id)
+        //주문버튼 누르면 마이페이지 내 주문목록 조회로 이동
+        const url = `${BACK_END_URL}/order/list/${product_id}/`
+        console.log(url)
+        const response = await fetch(url, {
+            headers: {
+                'content-type': 'application/json',
+                "Authorization": "Bearer " + localStorage.getItem("access")
+            },
+            method: 'POST',
+            body: JSON.stringify({
+                "price":String(amount_money),
+                "size":size,
+                "count":String(num)
+            })
+            })
+
+            if (response.status == 201) {
+                alert(response.status)
+            }
+            else {
+                alert(response.status)
+            }
+     
+}    
 //==================================================================    
 function selectButton() {
     const select_form = document.getElementById("select_form")
@@ -211,38 +261,4 @@ function downButton() {
         console.log(amount_money)
     }
 
-}
-
-function orderButton() {
-    console.log("주문 버튼 클릭")
-    console.log(num, size, amount_money)
-}
-
-function addcommend() {
-    // const commentText = document.getElementById('comment_input').value;
-    // const query = document.querySelector('input[name="rating"]:checked');
-    console.log(commentText, query.value)
-
-}
-
-// function deleteComment() {
-//     console.log("댓글 삭제")
-// }
-
-function editComment() {
-    console.log("댓글 수정")
-    const editBox = document.getElementById('edit_box')
-    if (editBox.style.display == 'none') {
-        editBox.style.display = 'block';
-    } else {
-        editBox.style.display = 'none';
-    }
-
-}
-
-function putComment() {
-    const editBox = document.getElementById('comment_edit').value
-    console.log(editBox)
-    alert("댓글 저장")
-    location.reload()
 }
